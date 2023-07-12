ed44e79e820359b38588a945c6cfcee21ea88883
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-07-12 13:02:24 -0600

    first commit

---
+++ POSWFs/Clases/CConexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+namespace POSWFs.Clases
+{
+    class CConexion
+    {
+        SqlConnection conex = new SqlConnection();
+        static string servidor = "localhost";
+        static string bd = "Sistema";
+        static string usuario = "sa";
+        static string password = "123456789";
+        static string puerto = "51170";
+
+        string cadenaConexion = "Data Source=" + servidor + "," + puerto+";"+ 
+            "user id=" + usuario + ";" + "password=" + password + ";" + 
+            "Initial Catalog=" + bd + ";" + "Persist Security Info=true";
+
+        public SqlConnection establecerConexion()
+        {
+            try
+            {
+                conex.ConnectionString = cadenaConexion;
+                conex.Open();
+                MessageBox.Show("Conexion establecida a la base de datos");
+            }catch(SqlException e)
+            {
+                MessageBox.Show("Error al conectar " + e.ToString());
+            }
+
+            return conex;
+        }
+    }
+}
---
+++ POSWFs/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace POSWFs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* try
+             {
+                 //Creando conexion a la bd
+                 SqlConnection conexion = new SqlConnection("Data Source=localhost;Initial Catalog=Sistema;Integrated Security=True");
+                 conexion.Open();
+                 MessageBox.Show("Conexion Exitosa");
+             } catch(Exception error)
+             {
+                 MessageBox.Show("Ha ocurrido un error. Intente nuevamente " + error.Message);
+             }*/
+
+            Clases.CConexion objetoConexion = new Clases.CConexion();
+            objetoConexion.establecerConexion();
+        }
+    }
+}

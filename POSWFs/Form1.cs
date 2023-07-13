ac9c9f7b5a14dd84a710dadead2d41d67f682895
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-07-12 18:45:32 -0600

    Table and DB correct connection

--- POSWFs/Form1.cs
+++ POSWFs/Form1.cs
@@ -22,6 +22,23 @@ namespace POSWFs
         private void button1_Click(object sender, EventArgs e)
         {
             //Biblioteca.Herramientas("Select * FROM Clientes where id = 1");
+            try
+            {
+                string validar = string.Format("Select * FROM Usuarios WHERE account='{0}' AND password='{1}'",txtUsuario.Text.Trim(),txtPass.Text.Trim());
+                DataSet conectar = Biblioteca.Herramientas(validar);
+
+                string cuenta = conectar.Tables[0].Rows[0]["account"].ToString().Trim();
+                string contrasena = conectar.Tables[0].Rows[0]["password"].ToString().Trim();
+
+                if (cuenta == txtUsuario.Text.Trim() && contrasena == txtPass.Text.Trim())
+                {
+                    MessageBox.Show("Inicio de sesion correcto");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Usuario o contrasena invalidos" + error);
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)

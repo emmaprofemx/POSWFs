c262a76bd7de21185869e47c66571c0da8218bfe
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-07-15 11:27:36 -0600

    DataSET programado para SQL

--- POSWFs/Consultas.cs
+++ POSWFs/Consultas.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using LibreriaDLL;
 namespace POSWFs
 {
     public partial class Consultas : FormBase
@@ -15,6 +15,32 @@ namespace POSWFs
         public Consultas()
         {
             InitializeComponent();
+        }
+
+       
+
+        public DataSet MostrarInfoDG(string tabla)
+        {
+            DataSet DS;
+            string cmd = string.Format("SELECT * FROM" + tabla);
+            //Obtenemos los datos y se mostrara en la tabla que hemos creado.
+            DS = Biblioteca.Herramientas(cmd);
+            return DS;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
         }
     }
 }

4a479471a299bbb419c5fd18a01339ad12b6ea97
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-07-24 11:54:29 -0600

    Instalando ReportViewer

--- POSWFs/Factura.Designer.cs
+++ POSWFs/Factura.Designer.cs
@@ -29,10 +29,17 @@ namespace POSWFs
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Factura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 749);
+            this.Name = "Factura";
             this.Text = "Factura";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
